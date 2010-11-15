004ebe39d493411893e306039b9366714006e360
root <root@vmdm0006.cern.ch>
2010-11-15 19:31:56 +0100

    added config variables to fill up templates, and more dpm nodes.

--- manifests/nodes.pp
+++ manifests/nodes.pp
@@ -2,6 +2,30 @@
 
 import 'dpm'
 
+#
+# Cluster configuration (global values)
+#
+$dpm_ns_host = "vmdm0001.cern.ch"
+$dpm_host = "vmdm0001.cern.ch"
+
+#
+# Nameserver configuration
+#
+$dpm_ns_dbuser = 'dpmmgr'
+$dpm_ns_dbpass = 'dpmmgr'
+$dpm_ns_dbhost = 'localhost'
+
+$dpm_ns_run = "yes"
+$dpm_ns_readonly = "no" 
+$dpm_ns_ulimit = 4096 
+$dpm_ns_coredump = "yes" 
+$dpm_ns_configfile = "/opt/lcg/etc/NSCONFIG" 
+$dpm_ns_logfile = "/var/log/dpns/log" 
+$dpm_ns_numthreads = 20 
+
+#
+# Node definition
+#
 node default {
 }
 
@@ -10,6 +34,5 @@ node slc5 inherits default {
 }
 
 node 'vmdm0001.cern.ch' inherits slc5 {
-	include dpm::dpns
+	include dpm::headnode
 }
-
--- modules/dpm/manifests/init.pp
+++ modules/dpm/manifests/init.pp
@@ -1,13 +1,36 @@
 # init.pp
 
-group { 'dpmmgr': gid => 151, ensure => present }
-user { 'dpmmgr': comment => 'dpm manager', uid => 151, gid => 'dpmmgr', ensure => present }
+group { 'dpmmgr': 
+	gid => 151, 
+	ensure => present,
+}
 
-class dpm::dpm {
+user { 'dpmmgr': 
+	comment => 'dpm manager', 
+	uid => 151, 
+	gid => 'dpmmgr', 
+	ensure => present,
+}
+
+class dpm {
+
+  class headnode {
+	include nameserver
+	include srm
+  }
+
+  class disknode {
+	include gridftp
+	include rfio
+  }
+
+  class dpm {
+
 	service { 'dpm':
 		subscribe => File['dpm-config', 'dpm-sysconfig'],
 		ensure => running,
 	}
+
 	file { 'dpm-config':
 		path => $operatingsytem ? {
 			default => '/opt/lcg/etc/DPMCONFIG',
@@ -17,6 +40,7 @@ class dpm::dpm {
 		mode  => 600,
 		content => template("dpm/dpm-config.erb"),
 	}
+
 	file { 'dpm-sysconfig':
 		name => $operatingsystem ? {
 			default => '/etc/sysconfig/dpm',
@@ -26,10 +50,13 @@ class dpm::dpm {
 		mode  => 644,
 		content => template("dpm/dpm-sysconfig.erb"),
 	}
-}
 
-class dpm::dpns {
-	package { ['DPM-name-server-mysql']: ensure => latest }
+  }
+
+  class nameserver {
+
+	package { ['DPM-name-server-mysql','vdt_globus_essentials']: ensure => latest }
+
 	service { 'dpns':
 		enable => true,
 		ensure => running,
@@ -38,9 +65,10 @@ class dpm::dpns {
 		name => 'dpnsdaemon',
 		subscribe => File['dpns-config', 'dpns-sysconfig'],
 	}
+
 	file { 'dpns-config':
 		name => $operatingsytem ? {
-			default => '/opt/lcg/etc/NSCONFIG',
+			default => $dpm_ns_configfile,
 		},
 		owner => dpmmgr,
 		group => root,
@@ -48,6 +76,7 @@ class dpm::dpns {
 		content => template("dpm/dpns-config.erb"),
 		recurse => inf,
 	}
+
 	file { 'dpns-sysconfig':
 		name => $operatingsystem ? {
 			default => '/etc/sysconfig/dpnsdaemon',
@@ -57,8 +86,35 @@ class dpm::dpns {
 		mode  => 644,
 		content => template("dpm/dpns-sysconfig.erb"),
 	}
-}
 
-class dpm::client {
+	file {
+		'dpns-logfile':
+		name => $operatingsystem ? {
+			default => $dpm_ns_logfile,
+		},
+		owner => dpmmgr,
+		group => dpmmgr,
+		mode  => 644,
+	}
+
+  }
+
+  class srm {
+
+  }
+
+  class gridftp {
+
+  }
+
+  class rfio {
+
+  }
+
+  class client {
+
 	package { 'dpm': ensure => latest }
+
+  }
+
 }
