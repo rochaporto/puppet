e55505468ba1e1dfaa8cf1b1147406c80c7d5fa9
root <root@vmdm0006.cern.ch>
2010-11-29 14:38:55 +0100

    Removed dependency on AFS for nodes in 'rocha'.
    Reason is that they run a custom kernel, for each there is no available afs module and it's too much trouble to maintain one.

--- manifests/rocha.pp
+++ manifests/rocha.pp
@@ -1,76 +1,76 @@
 # nodes.pp
 
-import 'cern'
-import 'dms'
-import 'dpm'
-import 'voms'
+import "cern"
+import "dms"
+import "dpm"
+import "voms"
 #
 # Client node(s)
 #
-node 'rocha-slc5.cern.ch' {
+node "rocha-slc5.cern.ch" {
 
-#
-# Cluster configuration (global values)
-#
-$dpm_ns_host = "localhost"
-$dpm_host = "localhost"
+    #
+    # Cluster configuration (global values)
+    #
+    $dpm_ns_host = "localhost"
+    $dpm_host = "localhost"
 
-#
-# DPM server configuration
-#
-$dpm_dbuser = 'dpmmgr'
-$dpm_dbhost = 'localhost'
-$dpm_run = "yes"
-$dpm_ulimit = 4096
-$dpm_coredump = "yes"
-$dpm_configfile = "/opt/lcg/etc/DPMCONFIG"
-$dpm_logfile = "/var/log/dpm/log"
-$dpm_gridmapdir = "/etc/grid-security/gridmapdir"
+    #
+    # DPM server configuration
+    #
+    $dpm_dbuser = "dpmmgr"
+    $dpm_dbhost = "localhost"
+    $dpm_run = "yes"
+    $dpm_ulimit = 4096
+    $dpm_coredump = "yes"
+    $dpm_configfile = "/opt/lcg/etc/DPMCONFIG"
+    $dpm_logfile = "/var/log/dpm/log"
+    $dpm_gridmapdir = "/etc/grid-security/gridmapdir"
 
-#
-# SRM server configuration
-#
-$dpm_srm_run = "yes"
-$dpm_srm_ulimit = 4096
-$dpm_srm_coredump = "yes"
-$dpm_srm_logfile = "/var/log/srmv2.2/log"
-$dpm_srm_gridmapdir = "/etc/grid-security/grid-mapfile"
-$dpm_srm_gridmapfile = "/etc/grid-security/gridmapdir"
+    #
+    # SRM server configuration
+    #
+    $dpm_srm_run = "yes"
+    $dpm_srm_ulimit = 4096
+    $dpm_srm_coredump = "yes"
+    $dpm_srm_logfile = "/var/log/srmv2.2/log"
+    $dpm_srm_gridmapdir = "/etc/grid-security/grid-mapfile"
+    $dpm_srm_gridmapfile = "/etc/grid-security/gridmapdir"
 
-#
-# RFIO server configuration
-#
-$dpm_rfio_run = "yes"
-$dpm_rfio_ulimit = 4096
-$dpm_rfio_logfile = "/var/log/rfio/log"
-$dpm_rfio_gridmapdir = "/etc/grid-security/grid-mapfile"
-$dpm_rfio_portrange = "20000 25000"
-$dpm_rfio_options = "-sl"
+    #
+    # RFIO server configuration
+    #
+    $dpm_rfio_run = "yes"
+    $dpm_rfio_ulimit = 4096
+    $dpm_rfio_logfile = "/var/log/rfio/log"
+    $dpm_rfio_gridmapdir = "/etc/grid-security/grid-mapfile"
+    $dpm_rfio_portrange = "20000 25000"
+    $dpm_rfio_options = "-sl"
 
-#
-# RFIO server configuration
-#
-$dpm_gsiftp_run = "yes"
-$dpm_gsiftp_logfile = "/var/log/dpm-gsiftp/dpm-gsiftp.log"
-$dpm_gsiftp_portrange = "20000 25000"
-$dpm_gsiftp_options = "-S -d all -p 2811 -auth-level 0 -dsi dpm -disable-usage-stats"
+    #
+    # RFIO server configuration
+    #
+    $dpm_gsiftp_run = "yes"
+    $dpm_gsiftp_logfile = "/var/log/dpm-gsiftp/dpm-gsiftp.log"
+    $dpm_gsiftp_portrange = "20000 25000"
+    $dpm_gsiftp_options = "-S -d all -p 2811 -auth-level 0 -dsi dpm -disable-usage-stats"
 
-#
-# Nameserver configuration
-#
-$dpm_ns_dbuser = 'dpmmgr'
-$dpm_ns_dbhost = 'localhost'
+    #
+    # Nameserver configuration
+    #
+    $dpm_ns_dbuser = 'dpmmgr'
+    $dpm_ns_dbhost = 'localhost'
 
-$dpm_ns_run = "yes"
-$dpm_ns_readonly = "no" 
-$dpm_ns_ulimit = 4096 
-$dpm_ns_coredump = "yes" 
-$dpm_ns_configfile = "/opt/lcg/etc/NSCONFIG" 
-$dpm_ns_logfile = "/var/log/dpns/log" 
-$dpm_ns_numthreads = 20 
+    $dpm_ns_run = "yes"
+    $dpm_ns_readonly = "no" 
+    $dpm_ns_ulimit = 4096 
+    $dpm_ns_coredump = "yes" 
+    $dpm_ns_configfile = "/opt/lcg/etc/NSCONFIG" 
+    $dpm_ns_logfile = "/var/log/dpns/log" 
+    $dpm_ns_numthreads = 20 
 
-# TODO: replace this with an exported resource, so that disk nodes simply publish themselves
-$disk_nodes = ["rocha-slc5.cern.ch"]
+    # TODO: replace this with an exported resource, so that disk nodes simply publish themselves
+    $disk_nodes = ["rocha-slc5.cern.ch"]
 
 
     Package { require => Yumrepo["dpm-mysql-unstable-etics", "epel"] }
@@ -83,8 +83,6 @@ $disk_nodes = ["rocha-slc5.cern.ch"]
     include dpm::headnode
     include dpm::disknode
     include dpm::client
-
-    cern::base::afs::user { ["rocha"]: }
 
     # setup supported domain/vo(s)
     dpm::headnode::domain { 'cern.ch': require => Service['dpns'], }
