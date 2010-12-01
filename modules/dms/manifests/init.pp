2c7c967a6d96caec1e22b30d00dd7571cec7beeb
root <root@vmdm0006.cern.ch>
2010-12-01 19:14:30 +0100

    Removed lcg-ca / epel repos (moved to cern).

--- modules/dms/manifests/init.pp
+++ modules/dms/manifests/init.pp
@@ -21,37 +21,19 @@ class dms {
 
         package { "lcgdm-libs": require => Package["yum-protectbase"] }
 
-        yumrepo { "dpm-mysql-unstable-etics":
-            descr    => "DPM MySQL ETICS Unstable Repository",
-            baseurl  => "http://etics-repository.cern.ch/repository/pm/volatile/repomd/name/lcgdm_unstable_sl5_x86_64_gcc412",
-            gpgcheck => 0,
-            enabled  => 1,
-            protect  => 1,
+        yumrepo { 
+            "dpm-mysql-unstable-etics":
+                descr    => "DPM MySQL ETICS Unstable Repository",
+                baseurl  => "http://etics-repository.cern.ch/repository/pm/volatile/repomd/name/lcgdm_unstable_sl5_x86_64_gcc412",
+                gpgcheck => 0,
+                enabled  => 1,
+                protect  => 1;
+            "glite-global-etics":
+                descr    => "ETICS Global gLite Registered Repository",
+                baseurl  => "http://etics-repository.cern.ch/repository/pm/registered/repomd/global/org.glite/sl5_x86_64_gcc412",
+                gpgcheck => 0,
+                enabled  => 1,
+                protect  => 0;
         }
-
-        yumrepo { "glite-global-etics":
-            descr    => "ETICS Global gLite Registered Repository",
-            baseurl  => "http://etics-repository.cern.ch/repository/pm/registered/repomd/global/org.glite/sl5_x86_64_gcc412",
-            gpgcheck => 0,
-            enabled  => 1,
-            protect  => 0,
-        }
-
-        yumrepo { "epel":
-            descr    => "Extra Packages for Enterprise Linux 5 - \$basearch",
-            baseurl  => "http://mirrors.fedoraproject.org/mirrorlist?repo=epel-5&arch=\$basearch",
-            gpgcheck => 0,
-            enabled  => 1,
-            protect  => 0,
-        }
-
-        yumrepo { "lcg-ca":
-            descr    => "LCG Certificate Authorities (CAs)",
-            baseurl  => "http://linuxsoft.cern.ch/LCG-CAs/current",
-            gpgcheck => 0,
-            enabled  => 1,
-        }
-
     }
-
 }
