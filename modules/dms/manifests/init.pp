02f025ae5d3a8070533df8e28c48bf59534322bc
root <root@vmdm0006.cern.ch>
2010-12-02 18:19:35 +0100

    Added lcgutil head etics repo.

--- modules/dms/manifests/init.pp
+++ modules/dms/manifests/init.pp
@@ -28,6 +28,12 @@ class dms {
                 gpgcheck => 0,
                 enabled  => 1,
                 protect  => 1;
+            "lcgutil-head-etics":
+                descr    => "LCGUTIL ETICS Unstable Repository",
+                baseurl  => "http://etics-repository.cern.ch/repository/pm/volatile/repomd/name/lcgutil_head_sl5_x86_64_gcc412",
+                gpgcheck => 0,
+                enabled  => 1,
+                protect  => 1;
             "glite-global-etics":
                 descr    => "ETICS Global gLite Registered Repository",
                 baseurl  => "http://etics-repository.cern.ch/repository/pm/registered/repomd/global/org.glite/sl5_x86_64_gcc412",
