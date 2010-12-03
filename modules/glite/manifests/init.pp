d6f970ebb4df706700d38f3836b1d8f9b2934d77
root <root@vmdm0006.cern.ch>
2010-12-03 16:00:38 +0100

    Fixed spaces/tabs.

--- modules/glite/manifests/init.pp
+++ modules/glite/manifests/init.pp
@@ -75,63 +75,63 @@ class glite {
 
     class gridmap {
 
-	    define mkgridmap($conffile, $mapfile, $logfile) {
+        define mkgridmap($conffile, $mapfile, $logfile) {
         
-    	    file { "$name-conf":
-        	    path    => $conffile,
-            	owner   => root,
-	            group   => root,
-    	        mode    => 644,
-        	    tag     => "gridmap",
-            	ensure  => present,
-	        }
+            file { "$name-conf":
+                path    => $conffile,
+                owner   => root,
+                group   => root,
+                mode    => 644,
+                tag     => "gridmap",
+                ensure  => present,
+            }
 
-    	    cron { "$name-cron":
-        	    command     => "(date; /opt/edg/libexec/edg-mkgridmap/edg-mkgridmap.pl --conf=$conffile --output=$mapfile --safe) >> $logfile 2>&1",
-            	environment => "PATH=/sbin:/bin:/usr/sbin:/usr/bin",
-	            user        => root,
-    	        hour        => [5,11,18,23],
-        	    minute      => 55,
-            	require     => [
-                	File["$name-conf"],
-	                Package["edg-mkgridmap"],
-    	        ],
-        	}
-	    }
+            cron { "$name-cron":
+                command     => "(date; /opt/edg/libexec/edg-mkgridmap/edg-mkgridmap.pl --conf=$conffile --output=$mapfile --safe) >> $logfile 2>&1",
+                environment => "PATH=/sbin:/bin:/usr/sbin:/usr/bin",
+                user        => root,
+                hour        => [5,11,18,23],
+                minute      => 55,
+                require     => [
+                    File["$name-conf"],
+                    Package["edg-mkgridmap"],
+                ],
+            }
+        }
 
-	    #
-    	# Manages entries in a mkgridmap config file. 
-	    # 
-    	# mkgridmap files provide the configuration of the edg-mkgridmap script, which
-	    # generates mapfiles by contacting the given VOMS URI, listing the users, and setting
-    	# a local mapping to the given 'map' value.
-	    #
-    	# Can be used for both pool account and VO mapping of DNs.
-	    #
-    	# $file:     The location of the mkgridmap config file to add the info to
-	    # $voms_uri: The URI of the VOMS server to be used when generating the mapfile
-    	# $map:      The value to map the DNs to
-	    #
-    	define group($file, $voms_uri, $map) {
-	        augeas { "vomsgroupadd_$file-$voms_uri-$map":
-    	        changes => [
-        	        "set /files$file/01/type group",
-            	    "set /files$file/01/uri $voms_uri",
-                	"set /files$file/01/map $map",
-	            ],
-    	        onlyif => "match /files$file/*[type='group' and uri='$voms_uri'] size == 0",
-        	    require => File[$file],
-	        }
-	        augeas { "vomsgroupupdate_$file-$voms_uri-$map":
-    	        changes => [
-        	        "set /files$file/*[type='group' and uri='$voms_uri']/map $map",
-            	],
-	            onlyif => "match /files$file/*[type='group' and uri='$voms_uri' and map!='$map'] size > 0",
-    	        require => File[$file],
-        	}
-	    }
+        #
+        # Manages entries in a mkgridmap config file. 
+        # 
+        # mkgridmap files provide the configuration of the edg-mkgridmap script, which
+        # generates mapfiles by contacting the given VOMS URI, listing the users, and setting
+        # a local mapping to the given 'map' value.
+        #
+        # Can be used for both pool account and VO mapping of DNs.
+        #
+        # $file:     The location of the mkgridmap config file to add the info to
+        # $voms_uri: The URI of the VOMS server to be used when generating the mapfile
+        # $map:      The value to map the DNs to
+        #
+        define group($file, $voms_uri, $map) {
+            augeas { "vomsgroupadd_$file-$voms_uri-$map":
+                changes => [
+                    "set /files$file/01/type group",
+                    "set /files$file/01/uri $voms_uri",
+                    "set /files$file/01/map $map",
+                ],
+                onlyif => "match /files$file/*[type='group' and uri='$voms_uri'] size == 0",
+                require => File[$file],
+            }
+            augeas { "vomsgroupupdate_$file-$voms_uri-$map":
+                changes => [
+                    "set /files$file/*[type='group' and uri='$voms_uri']/map $map",
+                ],
+                onlyif => "match /files$file/*[type='group' and uri='$voms_uri' and map!='$map'] size > 0",
+                require => File[$file],
+            }
+        }
     
-	    glite::gridmap::mkgridmap { "edg-mkgridmap":
+        glite::gridmap::mkgridmap { "edg-mkgridmap":
             conffile => "/opt/edg/etc/edg-mkgridmap.conf",
             mapfile  => "/etc/grid-security/grid-mapfile",
             logfile  => "/var/log/edg-mkgridmap.log",
