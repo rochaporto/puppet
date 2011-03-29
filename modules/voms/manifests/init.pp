ccf5b325ef24aaaf8009aa61a5504eadcad7b191
Ricardo Rocha <rocha.porto@gmail.com>
2011-03-29 14:19:00 +0200

    Repackaged the VOMS puppet module to follow the published guidelines.
    The new puppet style guide gives hints on how to organize resources inside
    the puppet manifests, but also the modules themselves.
    
    This is a first try at following this guidelines to get proper documentation,
    and a better structure for the resources inside the module.

--- modules/voms/manifests/init.pp
+++ modules/voms/manifests/init.pp
@@ -1,134 +1,71 @@
-# Class: voms
+# Class defining resources required to use the VOMS service.
+#
+# == Examples
 # 
-# This class provides definitions to manage a VOMS installation
+# Simply include this class:
+#   include voms
 #
-# Parameters:
+# And then enable one or more VOs by calling the 'vo' and 'server' definitions:
+#   voms::vo { "MyVO": }
 #
-# Actions:
+#   voms::server { 
+#    "voms_MyVO_cern":
+#      vo     => "MyVO",
+#      server => "voms.cern.ch",
+#      port   => 15001,
+#      dn     => ["/DC=ch/DC=cern/OU=computers/CN=voms.cern.ch"],
+#      ca_dn  => ["/DC=ch/DC=cern/CN=CERN Trusted Certification Authority"];
 #
-# Requires:
+# == Authors
 #
-# Sample Usage:
+# CERN IT/GT/DMS <it-dep-gt-dms@cern.ch>
 #
 class voms {
-    include glite
 
-    class client {
-        package { "glite-security-voms-clients": 
-            ensure  => latest, 
-            notify  => Exec["glite_ldconfig"],
-            require => Package["lcg-CA"],
-        }
+  file { 
+    "vomsdir":
+      ensure => directory,
+      path   => "/etc/grid-security/vomsdir",
+      owner  => root,
+      group  => root,
+      mode   => 644,
+      require => File["/etc/grid-security"];
+    "vomses":
+      ensure  => directory,
+      path    => "/opt/glite/etc/vomses",
+      owner   => root,
+      group   => root,
+      mode    => 755,
+      require => File["glite_etc"],
+  }
+
+  define vo {
+    file { "vomsdir_$name":
+      ensure  => directory,
+      path    => "/etc/grid-security/vomsdir/$name",
+      owner   => root,
+      group   => root,
+      mode    => 644,
+      require => File["vomsdir"],
     }
+  }
 
-    class base {
-        file { 
-            "vomsdir":
-                path   => "/etc/grid-security/vomsdir",
-                owner  => root,
-                group  => root,
-                mode   => 644,
-                ensure => directory,
-                require => File["/etc/grid-security"];
-            "vomses":
-                path    => "/opt/glite/etc/vomses",
-                owner   => root,
-                group   => root,
-                mode    => 755,
-                ensure  => directory,
-                require => File["glite_etc"],
-        }
-
-        define vo {
-            file { "vomsdir_$name":
-                path    => "/etc/grid-security/vomsdir/$name",
-                owner   => root,
-                group   => root,
-                mode    => 644,
-                ensure  => directory,
-                require => File["vomsdir"],
-            }
-        }
-
-        define server($vo, $server, $port, $dn, $ca_dn) {
-            file { 
-                "voms_lsc_$vo-$server":
-                    path    => "/etc/grid-security/vomsdir/$vo/$server.lsc",
-                    owner   => root,
-                    group   => root,
-                    mode    => 644,
-                    content => template("voms/lsc.erb"),
-                    require => File["vomsdir"];      
-                "vomses_$vo-$server":
-                    path    => "/opt/glite/etc/vomses/$vo-$server",
-                    owner   => root,
-                    group   => root,
-                    mode    => 644,
-                    content => template("voms/vomses.erb"),
-                    require => File["vomses"];
-            }
-        }
+  define server($vo, $server, $port, $dn, $ca_dn) {
+    file { 
+      "voms_lsc_$vo-$server":
+        path    => "/etc/grid-security/vomsdir/$vo/$server.lsc",
+        owner   => root,
+        group   => root,
+        mode    => 644,
+        content => template("voms/lsc.erb"),
+        require => File["vomsdir"];      
+      "vomses_$vo-$server":
+        path    => "/opt/glite/etc/vomses/$vo-$server",
+        owner   => root,
+        group   => root,
+        mode    => 644,
+        content => template("voms/vomses.erb"),
+        require => File["vomses"];
     }
-
-    class atlas {
-        include voms::base
-
-        voms::base::vo { "atlas": }
-
-        voms::base::server { 
-            "voms_atlas_cern":
-                vo     => "atlas",
-                server => "voms.cern.ch",
-                port   => 15001,
-                dn     => ["/DC=ch/DC=cern/OU=computers/CN=voms.cern.ch"],
-                ca_dn  => ["/DC=ch/DC=cern/CN=CERN Trusted Certification Authority"];
-            "voms_atlas_lcg":
-                vo     => "atlas",
-                server => "lcg-voms.cern.ch",
-                port   => 15001,
-                dn     => ["/DC=ch/DC=cern/OU=computers/CN=lcg-voms.cern.ch"],
-                ca_dn  => ["/DC=ch/DC=cern/CN=CERN Trusted Certification Authority"];
-        }
-    }
-
-    class dteam {
-        include voms::base
-
-        voms::base::vo { "dteam": }
-
-        voms::base::server {
-            "voms_dteam_cern":
-                vo     => "dteam",
-                server => "voms.cern.ch",
-                port   => 15001,
-                dn     => ["/DC=ch/DC=cern/OU=computers/CN=voms.cern.ch"],
-                ca_dn  => ["/DC=ch/DC=cern/CN=CERN Trusted Certification Authority"];
-            "voms_dteam_greece":
-                vo     => "dteam",
-                server => "voms.hellasgrid.gr",
-                port   => 15001,
-                dn     => ["/C=GR/O=HellasGrid/OU=hellasgrid.gr/CN=voms.hellasgrid.gr"],
-                ca_dn  => ["/C=GR/O=HellasGrid/OU=Certification Authorities/CN=HellasGrid CA 2006"];
-            "voms_dteam_greece2":
-                vo     => "dteam",
-                server => "voms2.hellasgrid.gr",
-                port   => 15001,
-                dn     => ["/C=GR/O=HellasGrid/OU=hellasgrid.gr/CN=voms2.hellasgrid.gr"],
-                ca_dn  => ["/C=GR/O=HellasGrid/OU=Certification Authorities/CN=HellasGrid CA 2006"];
-            "voms_dteam_lcg":
-                vo     => "dteam",
-                server => "lcg-voms.cern.ch",
-                port   => 15001,
-                dn     => ["/DC=ch/DC=cern/OU=computers/CN=lcg-voms.cern.ch"],
-                ca_dn  => ["/DC=ch/DC=cern/CN=CERN Trusted Certification Authority"];
-            "voms_dteam_tbed":
-                vo     => "dteam",
-                server => "lxbra2309.cern.ch",
-                port   => 15002,
-                dn     => ["/DC=ch/DC=cern/OU=computers/CN=lxbra2309.cern.ch"],
-                ca_dn  => ["/DC=ch/DC=cern/CN=CERN Trusted Certification Authority"];
-        }
-
-    }
+  }
 }
-
