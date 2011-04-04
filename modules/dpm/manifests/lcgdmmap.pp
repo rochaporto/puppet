77ccb2481df7b766b848c0fd59c99d15a83ec9b3
root <root@vmdm0006.cern.ch>
2011-04-04 13:45:22 +0200

    Removed modules directory (each individual module was moved to its own repo).

--- modules/dpm/manifests/lcgdmmap.pp
+++
@@ -1,7 +0,0 @@
-class dpm::lcgdmmap {
-  glite::gridmap::mkgridmap { "lcgdm-mkgridmap":
-    conffile => "/opt/lcg/etc/lcgdm-mkgridmap.conf",
-    mapfile  => "/opt/lcg/etc/lcgdm-mapfile",
-    logfile  => "/var/log/lcgdm-mkgridmap.log",
-  }
-}
