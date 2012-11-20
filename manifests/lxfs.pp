dd3af39d8ffa270db3ea2014017510677c4ad359
Ricardo Brito Da Rocha <rocha@rocha-puppet.cern.ch>
2012-11-20 16:52:49 +0100

    added lxfs cluster

--- manifests/lxfs.pp
+++ manifests/lxfs.pp
@@ -1,26 +1,24 @@
-# site.pp
-import 'lxfs.pp'
+# lxfs.pp
 
-node base {
+node lxfsbase {
 
-    package {"lcg-CA": ensure => latest }
+    include dms::cbuilds::repo
+    include dms::emi2::repos
 
-}
-
-node "rocha-el6.cern.ch" inherits base {
-
-    # dmlite configuration
+    # dmlite adapter
     class{
       "dmlite::plugins::adapter::config":
         dpm_host 		=> "lxfsra04a04.cern.ch",
         connection_timeout 	=> 10,
+        token_password 		=> "test",
+        token_id	 	=> "ip",
 	enable_rfio		=> true,
 	enable_io		=> false,
-	notify			=> Service["globus-gridftp-server"],
     }
     class{"dmlite::plugins::adapter::install":}
 
     # gridftp configuration
+    package{"dpm-dsi": ensure => latest}
     file { 
 	"/var/log/dpm-gsiftp": 
 		ensure => directory 
@@ -33,6 +31,7 @@ node "rocha-el6.cern.ch" inherits base {
 	log_single		=> "/var/log/dpm-gsiftp/gridftp.log",
 	log_transfer		=> "/var/log/dpm-gsiftp/dpm-gsiftp.log",
 	log_level		=> "ALL",
+        log_module		=> "stdio:buffer=0",
 	login_msg		=> "Disk Pool Manager (dmlite)",
 	port			=> 2811,
 	thread_model		=> "pthread",
@@ -40,9 +39,17 @@ node "rocha-el6.cern.ch" inherits base {
     }
     class{"gridftp::install":}
     class{"gridftp::service":}
+}
 
-    # webdav configuration
-    apache::mod {"ssl": }
-    class{"dmlite::apache::lcgdm_ns": }
+node "lxfsra04a04.cern.ch" inherits lxfsbase {
+
+    # dmlite s3
+    class{"dmlite::plugins::s3::config":}
+    class{"dmlite::plugins::s3::install":}
+
+}
+
+node "lxfsra12a06.cern.ch" inherits lxfsbase {
+
 
 }
